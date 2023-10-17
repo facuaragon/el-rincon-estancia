3eb9ddfbdc075b1d6b5d74d70f217971c9ab2504
facuaragon <facundo_aragon@hotmail.com>
2023-10-17 19:04:36 -0300

    add smooth scrolling to sections

--- app/page.js
+++ app/page.js
@@ -1,21 +1,21 @@
 export default function Home() {
   return (
     <>
-      <div id="nosotros" className="w-screen h-screen pt-[56px]">
+      <section id="nosotros" className="w-screen h-screen pt-[56px]">
         <h1>Nosotros</h1>
-      </div>
-      <div id="habitaciones" className="w-screen h-screen pt-[56px]">
+      </section>
+      <section id="habitaciones" className="w-screen h-screen pt-[56px]">
         <h1>Habitaciones</h1>
-      </div>
-      <div id="galeria" className="w-screen h-screen pt-[56px]">
+      </section>
+      <section id="galeria" className="w-screen h-screen pt-[56px]">
         <h1>Galeria</h1>
-      </div>
-      <div id="actividades" className="w-screen h-screen pt-[56px]">
+      </section>
+      <section id="actividades" className="w-screen h-screen pt-[56px]">
         <h1>Actividades</h1>
-      </div>
-      <div id="contacto" className="w-screen h-screen pt-[56px]">
+      </section>
+      <section id="contacto" className="w-screen h-screen pt-[56px]">
         <h1>Contacto</h1>
-      </div>
+      </section>
     </>
   );
 }
